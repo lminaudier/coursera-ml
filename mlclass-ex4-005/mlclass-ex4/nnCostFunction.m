95f5977a8555d7b01b8297b49c212930d3b9bb92
Loïc Minaudier <loic.minaudier@gmail.com>
2014-04-13 02:07:14 +0200

    Implement Ex5 of PE4

--- mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4-005/mlclass-ex4/nnCostFunction.m
@@ -120,8 +120,13 @@ for t = 1:m
   Theta2_grad = Theta2_grad + delta_3' * a2_t;
 endfor
 
+Theta1_grad_reg = (lambda / m) * Theta1(:, 2:end);
+Theta2_grad_reg = (lambda / m) * Theta2(:, 2:end);
+
 Theta1_grad = (1 / m) * Theta1_grad;
 Theta2_grad = (1 / m) * Theta2_grad;
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + Theta1_grad_reg;
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + Theta2_grad_reg;
 
 % -------------------------------------------------------------
 
